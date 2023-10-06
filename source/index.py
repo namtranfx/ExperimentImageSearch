10b92a91a0845fa9a5fa59311dbfea0f9eac3592
Nam Tran <namtranfx@gmail.com>
2023-10-06 10:41:00 +0700

    modify index system
    Modify base index class, modify old index and add new lsh index class

--- source/index.py
+++ source/index.py
@@ -24,11 +24,50 @@ class MyIndex:
 
     """
     def __init__(self) -> None:
-        pass
+        self.is_created = False
+        self.is_loaded = False
     def saveIndex(self, path):
-        pass
+        """
+        Preprocess the path before saving index file 
+
+        Need to re-implement this method for specific index
+        """
+        file_ver, newest_ver, isFound, path_component = self.checkIndexVer(path)
+        final_path = ""
+        if file_ver == 0: # no version is specified
+            if (newest_ver == -1): # no index file was saved before, no conflict here
+                final_path = path
+            else: # save index file with greater version than every file
+                final_path = path_component[0] + "-fx" + str(newest_ver + 1) + "." + path_component[1]
+        else: # Save index file with specific version, so save or override
+            final_path = path_component[0] + "-fx" + str(file_ver) + "." + path_component[1]
+        self.final_path = final_path
     def loadIndex(self, path):
-        pass
+        """
+        Preprocess the path before loading index file. 
+
+        Need to re-implement this method for specific index
+        """
+        file_ver, newest_ver, isFound, path_component = self.checkIndexVer(path)
+        print(file_ver, newest_ver, isFound)
+        final_path = ""
+        if file_ver == 0: # No version is specified, load newest version index file which was saved
+            if (newest_ver == -1): 
+                final_path = None
+                print("[WARNING]: Finding the index file: ", path)
+                print("[ERROR]: NO INDEX FILE FOUND!")
+                exit(-100)
+            elif newest_ver == 0: final_path = path
+            else: final_path = path_component[0] + "-fx" + str(newest_ver) + "." + path_component[1]
+
+        else: # Load the specific version file or load newest version file if not found
+            if isFound:
+                final_path = path_component[0] + "-fx" + str(file_ver) + "." + path_component[1]
+            else:
+                print("[WARNING]: No specific version found!") 
+                print("[WARNING]: Loading newest version file...") 
+                final_path = path_component[0] + "-fx" + str(newest_ver) + "." + path_component[1]
+        self.final_path = final_path
     def add(self, feature_vec):
         pass
     # Return an array of image indices
@@ -90,104 +129,172 @@ class MyIndex:
 from sparselsh import LSH
 from scipy.sparse import csr_matrix
 import pickle
+from lshashpy3 import LSHash
+import math
+class NewLSHIndex_old(MyIndex):
+    def __init__(self, bitdepth, input_dim, hashtable = 5) -> None:
+        super().__init__()
+        self.idx = 0
+        self.bitdepth = bitdepth
+        self.input_dim = input_dim
+        self.hash_table = hashtable
+
+        # hashtable_t = int(math.sqrt(input_dim)) if hashtable == 0 else hashtable
+        self.lsh = LSHash(hash_size=bitdepth, input_dim=input_dim, num_hashtables= hashtable)
+        
+
+    def add(self, feature_vec):
+        if self.is_loaded == True:
+            print("[WARNING]: Adding to the existed index. Deleting and re-initializing...")
+            del self.lsh
+            self.__init__(self.bitdepth, self.input_dim, self.hash_table)
+            self.is_loaded = False    
+        # print("index vector shape: ", feature_vec.shape)
+        self.lsh.index(feature_vec.flatten(), extra_data=self.idx)
+        self.idx = self.idx + 1
+    
+    def saveIndex(self, path):
+        super().saveIndex(path)
+        with open(self.final_path, "wb") as f:
+            pickle.dump(self.lsh, f)
+        self.is_loaded = True
+    
+    def loadIndex(self, path):
+        if self.is_loaded == True:
+            print("[WARNING]: Another index existed. Overriding new index!")
+            del self.lsh
+        super().loadIndex(path)
+        with open(self.final_path, "rb") as f:
+            self.lsh = pickle.load(f)
+
+    def search(self, query_feature_vec, k_top):
+        if self.is_loaded == False:
+            print("[WARNNG]: Querying in a empty index!")
+            return [None * k_top]
+        nearest_vectors = self.lsh.query(query_feature_vec.flatten(), num_results=k_top, distance_func="l1norm")
+        print("Number of retrieved elements: ", len(nearest_vectors))
+        nearest_indices = [vec[0][1] for vec in nearest_vectors]
+        return nearest_indices
 
 class NewLSHIndex(MyIndex):
-    def __init__(self, bitdepth, input_dim, hashtable = 1) -> None:
+    def __init__(self, bitdepth, input_dim, hashtable = 5) -> None:
         super().__init__()
-        count = 0
+        self.idx = 0
+        self.bitdepth = bitdepth
+        self.input_dim = input_dim
+        self.hash_table = hashtable
         self.lsh = LSH(
                     bitdepth,
                     input_dim,
                     num_hashtables=hashtable,
                     storage_config={"dict": None}
                 )
-    
-    def add(self, feature_vec, label):
-        self.lsh.index(feature_vec, extra_data=label)
+
+    def add(self, feature_vec, label = None):
+        if self.is_loaded == True:
+            print("[WARNING]: Adding to the existed index. Deleting and re-initializing...")
+            del self.lsh
+            self.__init__(self.bitdepth, self.input_dim, self.hash_table)
+            self.is_loaded = False
+        # self.lsh.index(feature_vec, extra_data=label)
+        self.lsh.index([feature_vec.flatten()], extra_data=self.idx)
+        self.idx = self.idx + 1
     def saveIndex(self, path):
-        with open(path + ".pkl", "wb") as f:
+        super().saveIndex(path)
+        with open(self.final_path, "wb") as f:
             pickle.dump(self.lsh, f)
+        self.is_loaded = True
+
     def loadIndex(self, path):
-        with open(path + ".pkl", "rb") as f:
+        if self.is_loaded == True:
+            print("[WARNING]: Another index existed. Overriding new index!")
+            del self.lsh
+        super().loadIndex(path)
+        with open(self.final_path, "rb") as f:
             self.lsh = pickle.load(f)
+        self.is_loaded = True
+
     def search(self, query_feature_vec, k_top):
-        points = self.lsh.query(query_feature_vec, num_results=k_top)
-        (point, label), dist = points[0:k_top - 1]
-        return label
+        if self.is_loaded == False:
+            print("[WARNNG]: Querying in a empty index!")
+            return [None * k_top]
+        points = self.lsh.query(query_feature_vec, num_results=k_top, distance_func="hamming")
+        index_points = [point[0][1] for point in points]
+        return index_points
 
 class FaissRawIndex(MyIndex):
     def __init__(self, feature_size) -> None:
         super().__init__()
         self.feature_size = feature_size
         self._faiss_index = faiss.IndexFlatL2(feature_size)   # build the index
-        self.is_finished = False
-    def saveIndex(self, path):
-        file_ver, newest_ver, isFound, path_component = self.checkIndexVer(path)
-        final_path = ""
-        if file_ver == 0: # no version is specified
-            if (newest_ver == -1): # no index file was saved before, no conflict here
-                final_path = path
-            else: # save index file with greater version than every file
-                final_path = path_component[0] + "-fx" + str(newest_ver + 1) + "." + path_component[1]
-        else: # Save index file with specific version, so save or override
-            final_path = path_component[0] + "-fx" + str(file_ver) + "." + path_component[1]
-        write_index(self._faiss_index, final_path)
-        self.is_finished = True
-    def loadIndex(self, path):
-        file_ver, newest_ver, isFound, path_component = self.checkIndexVer(path)
-        print(file_ver, newest_ver, isFound)
-        final_path = ""
-        if file_ver == 0: # No version is specified, load newest version index file which was saved
-            if (newest_ver == -1): 
-                final_path = None
-                print("[WARNING]: Finding the index file: ", path)
-                print("[ERROR]: NO INDEX FILE FOUND!")
-                exit(-100)
-            elif newest_ver == 0: final_path = path
-            else: final_path = path_component[0] + "-fx" + str(newest_ver) + "." + path_component[1]
-
-        else: # Load the specific version file or load newest version file if not found
-            if isFound:
-                final_path = path_component[0] + "-fx" + str(file_ver) + "." + path_component[1]
-            else:
-                print("[WARNING]: No specific version found!") 
-                print("[WARNING]: Loading newest version file...") 
-                final_path = path_component[0] + "-fx" + str(newest_ver) + "." + path_component[1]
-        self._faiss_index = read_index(final_path)
-    def add(self, feature_vec):
+        self.is_created = True
+    
         
-        if self.is_finished == True:
+    def saveIndex(self, path):
+        super().saveIndex(path)
+        write_index(self._faiss_index, self.final_path)
+        self.is_loaded = True
+    def loadIndex(self, path):
+        if self.is_loaded == True:
+            print("[WARNING]: Another index existed. Overriding new index!")
             self._faiss_index.reset()
-            self.is_finished = False
+        super().loadIndex(path)
+        self._faiss_index = read_index(self.final_path)
+        self.is_loaded = True
+    def add(self, feature_vec):
+        if self.is_loaded == True:
+            print("[WARNING]: Adding to the existed index. Deleting and re-initializing...")
+            self._faiss_index.reset()
+            self.is_loaded = False
         # print("[index.FaissRawIndex]: shape of feature vector indexing: ", feature_vec.shape)
         self._faiss_index.add(feature_vec) #add the representation to index
         
     def search(self, query_feature_vec, k_top = 5):
+        if self.is_loaded == False:
+            print("[WARNNG]: Querying in a empty index!")
+            return [None * k_top]
         _, I = self._faiss_index.search(query_feature_vec, k_top)
         return I[0][:]
 class FaissLSHIndex(FaissRawIndex):
     def __init__(self, feature_size, hash_width) -> None:
         super().__init__(feature_size)
         self._faiss_index = faiss.IndexLSH(feature_size, hash_width)
-class AnnoyLSHIndex(MyIndex):
+class CustomAnnoyIndex(MyIndex):
     # f_len : length of feature need to be indexed
     def __init__(self, f_len, num_tree = 10) -> None: 
         super().__init__()
+        self.feature_len = f_len
         self.num_tree = num_tree
         self._index = AnnoyIndex(f_len, 'hamming') # Create object AnnoyIndex use angular similarity
         self.idx = 0
     def add(self, feature_vec):
         # print("[index]: length of feature_vec = ", len(feature_vec[0]))
-        self._index.add_item(self.idx, feature_vec[0])
+        if self.is_loaded == True:
+            print("[WARNING]: Adding to the existed index. Deleting and re-initializing...")
+            del self.lsh
+            self.__init__(self.feature_len, self.num_tree)
+            self.is_loaded = False
+        self._index.add_item(self.idx, feature_vec.flatten())
         self.idx = self.idx + 1
     def saveIndex(self, path):
+        super().saveIndex(path)
         self._index.build(self.num_tree)
         self.idx = 0
         self._index.save(path + ".ann")
+        self.is_loaded = True
     def loadIndex(self, path):
+        if self.is_loaded == True:
+            print("[WARNING]: Another index existed. Overriding new index!")
+            del self.lsh
+            self.__init__(self.feature_len, self.num_tree)
+        super().loadIndex(path)
         self._index.load(path + ".ann")
+        self.is_loaded = True
     def search(self, query_feature_vec, k_top = 5):
-        similar_image_indices = self._index.get_nns_by_vector(query_feature_vec[0], k_top)
+        if self.is_loaded == False:
+            print("[WARNNG]: Querying in a empty index!")
+            return [None * k_top]
+        similar_image_indices = self._index.get_nns_by_vector(query_feature_vec.flatten(), k_top)
         return similar_image_indices
     
     
