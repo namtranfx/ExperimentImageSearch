d94558e11d7285921a9f4dd7a0cfb553861ad1be
Nam Tran <namtranfx@gmail.com>
2023-11-13 20:57:16 +0700

    Update ImageRetrieval_colab_auto.py

--- ImageRetrieval_colab_auto.py
+++ ImageRetrieval_colab_auto.py
@@ -117,8 +117,8 @@ BackBoneInstance = [
     # Resnet50Descriptor(), 
     # MobileNetV3Feature(),
     # MobileNetV3Feature_large(),
-    # SwinTransformer_default(),
-    tinyvit(),
+    SwinTransformer_default(),
+    # tinyvit(),
     # tinyvit_small(),
     # MyEfficientViT()
 ]
@@ -142,10 +142,10 @@ for idx in database_id:
     mydataloader.append(all_dataloader[idx])
 # Index system
 # feature_dim = [512, 2048, 576, 960, 768, 576]
-feature_dim = [576] # resnet18, resnet50, mobilenetv3_small, mobilenetv3_large, swin_vit, tiny_vit
+feature_dim = [768] # resnet18, resnet50, mobilenetv3_small, mobilenetv3_large, swin_vit, tiny_vit
 # feature_dim = [512] # resnet18, resnet50, mobilenetv3_small, mobilenetv3_large, swin_vit, tiny_vit
 RawIndex_bitdepth = [0]
-FaissLSH_bitdepth = [16, 32, 64, 128, 256, 512, 1024, 2048]
+FaissLSH_bitdepth = [1024, 2048]
 CustomLSH_bitdepth = [1, 2, 3, 4, 5, 6, 7, 8]
 bitdepth_config = [
     RawIndex_bitdepth,
